f6f7e6562aa800356382278d0a6d1c66c9b144eb
Sam Jayasinghe <sj2564@columbia.edu>
2015-01-31 22:33:26 -0500

    func(customer.js): modified customer schema

--- models/customer.js
+++ models/customer.js
@@ -3,7 +3,11 @@ var mongoose = restful.mongoose;
 
 var customerSchema = new mongoose.Schema({
   name: { first: String, last: String },
-  email: String
+  email: String,
+  address: [{ type: Schema.Types.ObjectId, ref: 'Addresses' }],
+  active: Boolean,
+  create_date: { type: Date },
+  last_update: { type: Date }
 });
 
 module.exports = restful.model('Customers', customerSchema);
