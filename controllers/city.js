3f3e123b8eaae9def9048300166cfffdf42487fc
bslakter <bslakter@dyn-129-236-238-141.dyn.columbia.edu>
2015-02-03 15:32:31 -0500

    feat(city.js): put function implemented

--- controllers/city.js
+++ controllers/city.js
@@ -14,6 +14,7 @@ exports.getCity = function(req, res, next) {
       } else {
         res.json(200, {
           name: data.name,
+          country: data.country,
           link: 'cities/' + data._id
         });
       }
@@ -29,15 +30,17 @@ exports.getCity = function(req, res, next) {
         for (i = 0; i < data.length; i++) {
           results[i] = {
             name: data[i].name,
+            country: data[i].country,
             link: 'cities/' + data[i]._id
           };
         }
         res.json(200, results);
       }
-    })
+    });
   }
 }
 
+// need to account for bad fields?
 exports.postCity = function(req, res, next) {
   var name = JSON.parse(req.body).name;
   var countryID = JSON.parse(req.body).country;
@@ -50,20 +53,46 @@ exports.postCity = function(req, res, next) {
         });
       } else {
         var city = new City();
-	    city.name = name;
-	    city.country = countryID;
-	    city.last_update = new Date();
-	    city.save(function () {
-	      res.send(req.body);
-	    });
+  	    city.name = name;
+  	    city.country = countryID;
+  	    city.last_update = new Date();
+  	    city.save(function () {
+  	      res.send(req.body);
+  	    });
       }
     });
   }
 }
 
 exports.putCity = function(req, res, next) {
-
+  if (!(req.params.id === undefined ||
+    req.params.id === '')) {
+    City.findById(req.params.id, function(err, data) {
+      if (err) {
+        res.json(500, {
+          message: 'Error occured: ' + err
+        });
+      } else {
+        var name = JSON.parse(req.body).name;
+        if (!(name === undefined ||name === '')) {
+          data.name = name;
+        }
+        //should we even allow for this?
+        var countryID = JSON.parse(req.body).country;
+        if (!(countryID === undefined || countryID === '')) {
+          data.country = countryID;
+        }
+        data.last_update = new Date();
+        res.json(200, {
+          name: data.name,
+          country: data.country,
+          link: 'cities/' + data._id
+        });
+      }
+    });
+  }
 }
+
 
 exports.delCity = function(req, res, next) {
   if (!(req.params.id === undefined ||
