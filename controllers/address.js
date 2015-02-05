28c597099580d10c125bb3c00ec15fb5f1b8ec5b
Samurdha Jayasinghe <samurdha@dyn-160-39-182-72.dyn.columbia.edu>
2015-02-04 22:09:14 -0500

    feat(address.js): finished skeleton

--- controllers/address.js
+++ controllers/address.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose'),
     Address = mongoose.model('Address');
 
-exports.getAddress = function (req, res, next) {
+module.exports.getAddress = function (req, res, next) {
   req.params.prev = 'Address';
 
   // If request passed through getCustomer
   if (req.params.prev !== undefined) {
-    var address = getAddressFromDB(req.params.Customer);
+    var address = getAddressFromDB(req.params.Customer[0].address_id);
     req.params.address = address;
     return next();
   }
@@ -14,8 +14,15 @@ exports.getAddress = function (req, res, next) {
   if (req.query.q !== undefined)
     return next();
 
-  var address = getAddressFromDB();
+  var address = getAddressFromDB(req.params.id);
   req.params.address = address;
   return next();
+
+  var getAddressFromDB = function (arg) {
+    if (arg !== undefined) {
+    
+    }
+  };
+
 }
 
