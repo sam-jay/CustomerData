829f6f04f07f2b88d39fe750d5ad7747d80530d0
bslakter <bslakter@dyn-129-236-238-50.dyn.columbia.edu>
2015-02-18 10:50:09 -0500

    feat(city.js): implemented put method

--- controllers/city.js
+++ controllers/city.js
@@ -33,21 +33,21 @@
         country_ID,
         queued_request;
 
-    /* Attempt to get country name from request body */
+    /* Attempt to get city name and country ID from request body */
     try { 
       city_name = JSON.parse(req.body).city;
       country_ID = JSON.parse(req.body).country_id;
     } catch (e) { 
       return error.respond(400, res, 'Cannot parse input');
     }
+    /* Validate parameters */
     if (validator.isNull(city_name) ||  validator.isNull(country_ID))
       return error.respond(400, res, 'Cannot parse input'); 
+    /* Ensure valid countryID given */
      Country.findById(country_ID, function(err, data) {
         if (err)
           return error.respond(404, res, '/api/countries/' + country_ID);
       });
-    // look for country ID
-      //country does not exit - respond with error messaage
     /* Add this request to the queue */
     queued_request = queue.push(10000);
     res.json(202, {
@@ -55,7 +55,7 @@
       url: '/api/queued_requests/' + queued_request._id
     });
 
-        /* Save new country */
+    /* Save new city */
     var city = new City();
     city.city = city_name;
     city.country_id = country_ID;
@@ -66,6 +66,52 @@
       queued_request.setStatus('Success');
       return queued_request.setResource('/api/cities/' 
         + city._id);
+    });
+  }
+
+  exports.putCity = function(req, res, next) {
+    var city_name,
+        country_ID,
+        parsedBody,
+        queued_request;
+
+    /* City Name or Country ID could be updated */
+    try {
+      parsedBody = JSON.parse(req.body);
+    } catch (e) {
+      return error.respond(400, res, 'Cannot parse input');
+    }
+    if (!parsedBody.city && !parsedBody.country_id) {
+      return error.respond(400, res, 'Cannot parse input');
+    }
+
+    /* Look up City */
+    City.findById(req.params.id, function(err, city) {
+      if (err)
+        return queued_request.setStatus('Failed');
+      console.log(city);
+      queued_request = queue.push(10000);
+      res.json(202, {
+        message: 'Resource accepted (Operation Pending)',
+        url: '/api/queued_requests/' + queued_request._id
+      });
+
+      /* Update country */
+      if (parsedBody.city)
+        city.city = parsedBody.city;
+      if (parsedBody.country_id) 
+        city.country_id = parsedBody.country_id;
+      city.last_update = new Date();
+
+      /* Save */
+      city.save(function (err, city) {
+        if (err)
+          return queued_request.setStatus('Failed');
+
+        queued_request.setStatus('Success');
+        return queued_request.setResource('/api/cities/'
+          + city._id);
+      });
     });
   }
 
