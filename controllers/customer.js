f228a4f8e348f23291d42d438fca3782b2576677
Samurdha Jayasinghe <sj2564@columbia.edu>
2015-02-19 12:48:04 -0500

    debug(customer.js)

--- controllers/customer.js
+++ controllers/customer.js
@@ -6,147 +6,70 @@ var mongoose = require('mongoose'),
     queue = require('./queue.js'),
     error = require('./error.js');
 
-
-
 exports.getCustomer = function(req, res, next) {
-  req.params.prev = 'Customer';
+    req.params.prev = 'Customer';
 
-  // If query go to next
-  if (req.query.q !== undefined) {
-    return next();
-  }
-
-  var getCustomerFromDB = function (arg) {
-    if (arg !== undefined) {
-      Customer.findById(arg, function(err, data) {
-      	if (err)
-      		return error.respond(404, res, '/api/customers/' + arg);
-      	req.params.pretty = 'true';
-      	req.params.data = data;
-      	return next();
+    /* If ID is given, find city */
+    if (req.params.id !== undefined) {
+      Customer.findById(req.params.id, function(err, data) {
+        if (err)
+          return error.respond(404, res, '/api/customers/' + req.params.id);
+        req.params.pretty = 'true';
+        req.params.data = data;
+        req.params.id = data.address_id;
+        return next();
       });
-    }
-  };
 
-  var customer = getCustomerFromDB(req.params.id);
-  req.params.customer = customer;
-  return next(); 
-}
-  /*
-  console.log(req.params);
-	if (!(req.params.id === undefined ||
-    req.params.id === '')) {
-		Customer.findById(req.params.id, function(err, data) {
-			if (err) {
-				res.json(500, {
-					message: "Error occured: " + err
-				});
-			} else {
-				res.json(200, {
-					name: data.name.first + ' ' + data.name.last,
-					email: data.email,
-					// active: data.active,
-					link: 'customers/' + data._id
-				});
-			}
-		});
+    /* Otherwise call the next function */
     } else {
-    	Customer.find({}, function(err, data) {
-    		if (err) {
-    			res.json(500, {
-    				message: "Error occured: " + err
-    			});
-    		} else {
-    			var i, results = [];
-    			for (i = 0; i < data.length; i++) {
-    				results[i] = {
-    					name: data[i].name.first + ' ' + data[i].name.last,
-    					link: 'customers/' + data[i]._id
-    				};
-    			}
-    			res.json(200, results);
-    		}
-    	});
+      return next();
     }
-*/
+}
 
 exports.postCustomer = function(req, res, next) {
+    var first_name = req.body.first_name,
+        last_name = req.body.last_name,
+        email = req.body.email,
+        address_id = req.body.address_id,
+        active = req.body.active,
+        queued_request;
 
-	var customer_fname, customer_lname,
-		customer_email,
-		customer_status,
-		address_url, address_ID,
-		queued_request;
+    if (validator.isNull(first_name) ||
+    	validator.isNull(last_name) ||
+    	validator.isNull(email) ||
+    	validator.isNull(address_id) ||
+    	validator.isNull(active))
+    	return error.respond(400, res, 'Cannot parse input'); 
 
-	// Parse request body
-	try {
-		var body = JSON.parse(req.body);
-		customer_fname = body.name.first;
-		customer_lname = body.name.last;
-		customer_email = body.email;
-		address_ID = body.address.split('/')[3];
-		customer_status = body.active;
-	} catch (e) { return error.respond(400, res, 'Cannot parse input'); }
+    /* Ensure valid countryID given */
+    Address.findById(address_id, function(err, data) {
+      if (err)
+        return error.respond(404, res, '/api/addresses/' + address_id);
+      if (data === null)
+        return error.respond(404, res, '/api/addresses/' + address_id);
+      /* Add this request to the queue */
+      queued_request = queue.push(10000);
+      res.json(202, {
+        message: 'Resource accepted (Operation Pending)',
+        url: '/api/queued_requests/' + queued_request._id
+      });
 
-	console.log(
-		"name: " + customer_lname + ', ' + customer_fname + '\n' +
-		"email: " + customer_email + '\n' +
-		"address_id: " + address_ID + '\n' +
-		"customer_status: " + customer_status + '\n');
+      /* Save new city */
+      var customer = new Customer();
+      customer.first_name = first_name;
+      customer.last_name = last_name;
+      customer.email = email;
+      customer.address_id = address_id;
+      customer.active = active;
+      customer.save(function(err, customer) {
+        if (err)
+          return queued_request.setStatus('Failed');
 
-	// Validate parameters here
-
-	// Ensure valid address_id here
-	Address.findById(address_ID, function(err, data) {
-
-		console.log("looking up address");
-		if (err)
-	        return error.respond(404, res, '/api/addresses/' + address_ID);
-	    if (data === null)
-	        return error.respond(404, res, '/api/addresses/' + address_ID);
-	    
-		// Add this request to queue
-		queued_request = queue.push(10000);
-		res.json(202, {
-			message: 'Resource accepted (Operation Pending)',
-	        url: '/api/queued_requests/' + queued_request._id
-		});
-
-		// Save new customer
-		var customer = new Customer();
-		customer.first_name = customer_fname;
-		customer.last_name = customer_lname;
-		customer.email = customer_email;
-		customer.address_id = address_ID;
-		customer.active = customer_status;
-		customer.save(function(err, customer) {
-			if (err) return queued_request.setStatus('Failed');
-			queued_request.setStatus('Success');
-			return queued_request.setResource('/api/customers/' + customer._id);
-		});
-	});
-	// if (!(body.name === undefined ||
-	// 	body.name === '')) {
-	// 	var customer = new Customer();
-	// 	// customer.name = body.name;
-	// 	var names = body.name.split(' ');
-	// 	if (names.length != 2) {
-	// 		res.json(500, {
-	// 			message: "Error occured: must send 2 names"
-	// 		});
-	// 	}
-	// 	customer.name = {
-	// 		first: names[0],
-	// 		last: names[1]
-	// 	};
-	// 	customer.last_update = new Date();
-	// 	customer.create_date = customer.last_update;
-	// 	customer.email = body.email;
-	// 	customer.save( function() {
-	// 		console.log("Saving: " + customer);
-	// 		res.send(customer);
-	// 	});
-	// }
+        queued_request.setStatus('Success');
+        return queued_request.setResource('/api/customers/' 
+          + customer._id);
+      });
+    });
 }
 
 exports.delCustomer = function(req, res, next) {
